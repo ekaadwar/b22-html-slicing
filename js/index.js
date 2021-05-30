5005a9d3c071f2caa6eed1dce6a3d3becd962860
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-05-31 06:30:04 +0700

    js/index.js - test the express get mathod with '/' endpoint

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  const data = {
+    success: true,
+    message: "halo World",
+  };
+  return res.json(data);
+});
+
+// const routeItems = require("./src/routes/items");
+
+app.listen(8080, () => {
+  console.log("App running ini port 8080");
+});
